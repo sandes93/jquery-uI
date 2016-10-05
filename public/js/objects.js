acd87a90cab5b11f454c41270a1a9423c631348d
Sandesh Pathak <sandeshpathak0112@gmail.com>
2016-10-05 12:33:38 -0400

    12:33 update

--- public/js/objects.js
+++ public/js/objects.js
@@ -1,5 +1,8 @@
 var selections = {'#hotel-choices': hotel, '#restaurant-choices':restaurant, '#activity-choices': activity};
 
+var interactiveObject = [{hotel:[],restaurant:[],activity:[]}];
+
+
 $(document).ready(function() {
     initializeMap();
 
@@ -19,21 +22,38 @@ $(document).ready(function() {
 
 $(".btn-primary").click(function() {
 
+
+
    //console.log( "Handler for .click() called." );
    var $category = $(this).closest( "div" ).find("select").attr('id');
+
    var $result = $( "#"+$category+" option:selected" ).text();
    $('#'+$category.split("-")[0]).append($('<div class="itinerary-item"><span class="title">'+$result+'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>'));
-   // console.log($result)
-   // console.log($category)
+    // console.log($result)
+    // console.log($category)
    var options = selections['#'+$category]
+var currentDay = ($('.day-buttons').children().length)-1;
 
    for (var i = 0; i < options.length; i++) {
        if (options[i].name === $result){
            locArr = options[i].place.location;
            drawMarker($category.split('-')[0], locArr)
-           //onsole.log(locArr)
-           break
+           //console.log(options[i])
+
+           // if(interactiveObject.length < currentDay){
+           //    interactiveObject.push({hotel:[],restaurant:[],activity:[]})
+           // }
+           console.log(interactiveObject);
+           interactiveObject[currentDay-1][$category.split("-")[0]].push(options[i]);
+           console.log(interactiveObject);
+           
+
+          
+
+
+            break;
        }
+
    }
 
    });
@@ -41,11 +61,21 @@ $(".btn-primary").click(function() {
 $(document).on('click', '.remove', function(){
     // console.log($(this).closest('div').find("span").empty());
     console.log($(this).closest('div').empty());
+    var count = $().closest('div').children().length
+     interactiveObject[$(this).closest('h4').attr('id')].splice(count,1);
 });
 
 $('#day-add').click(function(){
-    console.log($(this).closest('div').last().text())
-    $('<button class="btn btn-circle day-btn">!!</button>').insertBefore($(this))
-    // $(this).closest('div').append('<button class="btn btn-circle day-btn">!!</button>')
-    // console.log('works')
+    //console.log($(this).closest('div').last().text())
+    var count =  $(this).closest('div').children().length
+    interactiveObject.push({hotel:[],restaurant:[],activity:[]})
+
+   $('<button class="btn btn-circle day-btn">'+count+'</button>').insertBefore($(this))
+
+
+    
 });
+
+
+
+
